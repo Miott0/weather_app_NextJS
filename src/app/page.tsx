7b24e8a77a7477bea573c36330aeba352a16fe54
Rafael Miotte <88847519+Miott0@users.noreply.github.com>
2024-08-06 12:40:32 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ export default function Home(): JSX.Element {
   async function fetchData(cityName: string): Promise<void> {
     try {
       const response = await fetch(
-        `https://dev-miott-weatherapp.vercel.app/api/weather?address=${cityName}`
+        `https://miott-weater-app.vercel.app/api/weather?address=${cityName}`
       );
       const jsonData = (await response.json()).data;
       setWeatherData(jsonData);
